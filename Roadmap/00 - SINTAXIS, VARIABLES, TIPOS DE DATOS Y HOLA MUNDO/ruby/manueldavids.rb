4d33f1cfd55f7222d8622690714597c38f22f16f
manueldavids <manuelsantanadev@proton.me>
2025-09-07 07:47:53 -0400

    Learning de Fundamentals of Ruby Thanks to mirudev

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/ruby/manueldavids.rb
@@ -0,0 +1,27 @@
+# First comment in ruby https://www.ruby-lang.org/es/about/
+
+=begin
+This is my first
+multi-line comment
+=end
+
+# Variable declaration
+my_variable = "This is my first variable type string"
+
+# Constant Variable
+
+UPPERCASE_CONSTANT = "This is a constant"
+
+# Data types
+
+my_integer = 1 
+
+my_float = 1.5
+
+my_boolean = true
+
+my_string = "Manuel"
+
+# Print to screen
+
+puts "Hello, Ruby!"
